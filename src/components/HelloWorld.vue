2b9c78e9565efa7769099bf4ded6686f642471b9
Choi Sang Il <wo566@naver.com>
2023-02-18 15:05:01 +0900

    feat: 좌석예약

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
-    <button @click="send">click me</button>
+    <template v-for="(item, index) in seatList" :key="item">
+      <template v-if="index % 10 == 0"> <br/> </template>
+      <v-chip v-if="item" @click="cancle(index)">
+        {{ item }}
+      </v-chip>
+
+      <v-chip v-else-if="item == false" color="green" @click="reservation(index)">
+        {{ item }}
+      </v-chip>
+    </template>
+    
   </div>
 </template>
 
 <script>
 import Stomp from 'webstomp-client'
 import SockJS from 'sockjs-client'
+import axios from 'axios';
 
 export default {
   name: 'App',
   data() {
     return {
+      seatList: Array.from({length: 100}, () => false),
+      name: 'kukaro',
+      age: 26,
     }
   },
   created() {
@@ -20,13 +34,16 @@ export default {
     this.stompClient = Stomp.over(socket);
     
     console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+    let ref = this
     this.stompClient.connect(
         {},
         frame => {
           this.connected = true;
           console.log('소켓 연결 성공', frame);
-            this.stompClient.subscribe("/topic", res => {
-                console.log('구독으로 받은 메시지 입니다.', res.body);
+            this.stompClient.subscribe("/seat", res => {
+                console.log('구독으로 받은 메시지 입니다.', res.body)
+                let seat = JSON.parse(res.body)
+                ref.seatList[seat['id']] = seat['hasReservation']
             });
         },
         error => {
@@ -36,10 +53,27 @@ export default {
     );        
   },
   methods:{
-    send() {
-        if (this.stompClient && this.stompClient.connected) {
-            this.stompClient.send("/send", JSON.stringify("test"), {});
-        }
+    reservation(seatId) {
+      axios.get( "/api/v1/reservation/" + seatId)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error)=>{
+            console.log(error)
+            alert("서버가 아파요 ㅜㅜ")
+        })
+    },
+    cancle(seatId){
+      if(confirm(seatId + "취소하시겠습니까?")){
+        axios.get( "/api/v1/reservation/cancel/" + seatId)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error)=>{
+            console.log(error)
+            alert("서버가 아파요 ㅜㅜ")
+        })
+      }
     }
   }
 }
